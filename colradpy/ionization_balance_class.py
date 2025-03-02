Fixed bug where QCD/XCD rates were 1 along diagonal, causing numerical issues in ionization balance matrix

colradpy/ionization_balance_class.py
@@ -153,26 +153,60 @@ class ionization_balance():
                         self.data['input_file']['scd'] = adf11['input_file']
                         
                     if 'acd' in file:
-                        self.data['cr_data']['gcrs'][str(j)]['acd'] = interp_rates_adf11(adf11['input_file']['temp_grid'],
-                                                                                         adf11['input_file']['dens_grid'],
-                                                                          temp_grid,dens_grid,adf11['input_file'][str(j)])
+                        self.data['cr_data']['gcrs'][str(j)]['acd'] = interp_rates_adf11(
+                            adf11['input_file']['temp_grid'],
+                            adf11['input_file']['dens_grid'],
+                            temp_grid,
+                            dens_grid,
+                            adf11['input_file'][str(j)],
+                        )
                         self.data['input_file']['acd'] = adf11['input_file']
                         
                     if 'qcd' in file:
-                        self.data['cr_data']['gcrs'][str(j)]['qcd'] = interp_rates_adf11(adf11['input_file']['temp_grid'],
-                                                                                        adf11['input_file']['dens_grid'],
-                                                                         temp_grid,dens_grid,adf11['input_file'][str(j)])
+                        # Interpolator cannot handle negative infinities along
+                        # diagonal (caused by log of zero rate). Convert the
+                        # diagonal to zero before and after interpolation as a
+                        # workaround
+                        n = adf11['input_file'][str(j)].shape[0]
+                        adf11['input_file'][str(j)][np.diag_indices(n)] = 0
+                        self.data['cr_data']['gcrs'][str(j)]['qcd'] = interp_rates_adf11(
+                            adf11['input_file']['temp_grid'],
+                            adf11['input_file']['dens_grid'],
+                            temp_grid,
+                            dens_grid,
+                            adf11['input_file'][str(j)],
+                        )
+                        # Rates along diagonal get set to 1 due to
+                        # exponentiation inside interpolator, so set back to 0
+                        self.data['cr_data']['gcrs'][str(j)]['qcd'][np.diag_indices(n)] = 0
                         self.data['input_file']['qcd'] = adf11['input_file']
                         
                     if 'xcd' in file:
-                        self.data['cr_data']['gcrs'][str(j)]['xcd'] = interp_rates_adf11(adf11['input_file']['temp_grid'],
-                                                                                        adf11['input_file']['dens_grid'],
-                                                                         temp_grid,dens_grid,adf11['input_file'][str(j)])
+                        # Interpolator cannot handle negative infinities along
+                        # diagonal (caused by log of zero rate). Convert the
+                        # diagonal to zero before and after interpolation as a
+                        # workaround
+                        n = adf11['input_file'][str(j)].shape[0]
+                        adf11['input_file'][str(j)][np.diag_indices(n)] = 0
+                        self.data['cr_data']['gcrs'][str(j)]['xcd'] = interp_rates_adf11(
+                            adf11['input_file']['temp_grid'],
+                            adf11['input_file']['dens_grid'],
+                            temp_grid,
+                            dens_grid,
+                            adf11['input_file'][str(j)],
+                        )
+                        # Rates along diagonal get set to 1 due to
+                        # exponentiation inside interpolator, so set back to 0
+                        self.data['cr_data']['gcrs'][str(j)]['xcd'][np.diag_indices(n)] = 0
                         self.data['input_file']['xcd'] = adf11['input_file']                               
                     if 'ccd' in file:
-                        self.data['cr_data']['gcrs'][str(j)]['ccd'] = interp_rates_adf11(adf11['input_file']['temp_grid'],
-                                                                                        adf11['input_file']['dens_grid'],
-                                                                         htemp_grid,hdens_grid,adf11['input_file'][str(j)])
+                        self.data['cr_data']['gcrs'][str(j)]['ccd'] = interp_rates_adf11(
+                            adf11['input_file']['temp_grid'],
+                            adf11['input_file']['dens_grid'],
+                            htemp_grid,
+                            hdens_grid,
+                            adf11['input_file'][str(j)],
+                        )
                         self.data['input_file']['ccd'] = adf11['input_file']                        
                         
                     if 'qcd' not in self.data['cr_data']['gcrs'][str(j)]:

colradpy/read_adf11.py
@@ -48,33 +48,41 @@ def read_adf11(fil):
         num_stages = adf11['input_file']['nuc_charge']
     else:
         num_stages = len(adf11['input_file']['metas'])
-    for i in range(0,num_stages):
+    for i in range(num_stages):
         adf11['input_file'][str(i)] = {}
+        # Initialize matrices of rate coefficients. Since ADF11 data is stored
+        # as the base-10 log of the rate, the matrices should be initialized to
+        # negative infinity (so converting to a rate gives zero).
         if any(x in file_type for x in ['scd', 'acd', 'ccd']):
-            adf11['input_file'][str(i)] = np.zeros((adf11['input_file']['metas'][i],
-                                                     adf11['input_file']['metas'][i+1],
-                                             len(adf11['input_file']['temp_grid']),
-                                             len(adf11['input_file']['dens_grid'])))
-        if( 'qcd' in file_type ):
-            adf11['input_file'][str(i)] = np.zeros((adf11['input_file']['metas'][i],
-                                                     adf11['input_file']['metas'][i],
-                                             len(adf11['input_file']['temp_grid']),
-                                             len(adf11['input_file']['dens_grid'])))
-        if('xcd' in file_type):
-            adf11['input_file'][str(i)] = np.zeros((adf11['input_file']['metas'][i+1],
-                                                     adf11['input_file']['metas'][i+1],
-                                             len(adf11['input_file']['temp_grid']),
-                                             len(adf11['input_file']['dens_grid'])))
-        if('plt' in file_type):
-            adf11['input_file'][str(i)] = np.zeros((adf11['input_file']['metas'][i],
-                                                     adf11['input_file']['metas'][i],
-                                             len(adf11['input_file']['temp_grid']),
-                                             len(adf11['input_file']['dens_grid'])))
-        if('prb' in file_type):
-            adf11['input_file'][str(i)] = np.zeros((adf11['input_file']['metas'][i],
-                                                     adf11['input_file']['metas'][i],
-                                             len(adf11['input_file']['temp_grid']),
-                                             len(adf11['input_file']['dens_grid'])))
+            adf11['input_file'][str(i)] = np.full(
+                (
+                    adf11['input_file']['metas'][i],
+                    adf11['input_file']['metas'][i+1],
+                    len(adf11['input_file']['temp_grid']),
+                    len(adf11['input_file']['dens_grid']),
+                ),
+                -np.inf,
+            )
+        if any(x in file_type for x in ['qcd', 'plt', 'prb']):
+            adf11['input_file'][str(i)] = np.full(
+                (
+                    adf11['input_file']['metas'][i],
+                    adf11['input_file']['metas'][i],
+                    len(adf11['input_file']['temp_grid']),
+                    len(adf11['input_file']['dens_grid']),
+                ),
+                -np.inf,
+            )
+        if 'xcd' in file_type:
+            adf11['input_file'][str(i)] = np.full(
+                (
+                    adf11['input_file']['metas'][i+1],
+                    adf11['input_file']['metas'][i+1],
+                    len(adf11['input_file']['temp_grid']),
+                    len(adf11['input_file']['dens_grid']),
+                ),
+                -np.inf,
+            )
 
     #Reading the GCR value portion
     gcr_line = f.readline()